6e4b943e75bf670200eed618693318ef348a6682
Goodness Ezeokafor <gootech442@yahoo.com>
2019-12-04 00:39:30 +0100

    added unit tests to check of the contract deployed successfully

--- client/test/ProductDapp.test.js
+++ client/test/ProductDapp.test.js
@@ -2,3 +2,29 @@ const ProductDapp = artifacts.require('./ProductDapp.sol')
 
 
 
+contract("ProductDapp", ([deployer, seller, buyer])=> {
+    // console.log(deployer)
+    // console.log(seller)
+    // console.log(buyer)
+    
+
+    // assigns the deployed contract to this.shop
+    before(async() => {
+      this.shop =  await ProductDapp.deployed()  // calls the deployed contract
+    })
+
+
+    // checks if the contract deployed successfully
+    it("", async() => {
+        const address = this.shop.address
+        // console.log("Contract address:",address)
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, '')
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+    })
+
+
+    //
+
+})
