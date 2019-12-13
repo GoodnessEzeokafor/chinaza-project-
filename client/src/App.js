8d9ca7d5f442df9f6c38de147bff53770ce87156
Goodness Ezeokafor <gootech442@yahoo.com>
2019-12-13 21:16:33 +0100

    chFarmChainNg

--- client/src/App.js
+++ client/src/App.js
@@ -142,7 +142,9 @@ constructor(props){
         {/* Nav Bar */}
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
-            {this.state.productDappName}
+            {/* {this.state.productDappName}
+             */}
+             FarmChainNG
           </a>
           <input 
               className="form-control form-control-dark w-100" 
